01c5a3eecfe3c655561a42d0914916e1db9dd246
Max Kohler <hello@maxkohler.com>
2024-09-13 15:57:18 +0200

    Block out autocomplete component

--- src/components/Autocomplete/Autocomplete.stories.js
+++ src/components/Autocomplete/Autocomplete.stories.js
@@ -1,4 +1,4 @@
-import { userEvent, within, expect } from "@storybook/test"
+import { userEvent, within, expect, fn } from "@storybook/test"
 import AutoComplete from "."
 
 export default {
@@ -15,10 +15,46 @@ const testData = ["Apples", "Oranges", "Pears", "Peaches", "Bananas"].map((el) =
   }
 })
 
+const onSelect = fn((e) => {
+  return e.detail
+})
+
 export const Basic = {
+  parameters: {
+    docs: {
+      story: {
+        autoplay: true,
+      },
+    },
+  },
   args: {
     data: testData,
-    query: "apples",
+    query: "",
     placeholder: "Select a fruit",
+    event_select: onSelect,
+  },
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement)
+    const input = canvas.getByTestId("autocomplete-input")
+    await step("Select using the mouse", async () => {
+      await userEvent.click(input)
+      expect(input).toHaveFocus()
+      await userEvent.keyboard("ba")
+      const bananasOption = canvas.getByText("Bananas")
+      await userEvent.click(bananasOption)
+      expect(input).toHaveValue("Bananas")
+      expect(onSelect).toHaveReturnedWith({ item: { label: "Bananas", value: "Bananas", lowerCasedValue: "bananas", details: {} } })
+    })
+    await userEvent.clear(input)
+    await step("Select using the keyboard", async () => {
+      await userEvent.click(input)
+      expect(input).toHaveFocus()
+      await userEvent.keyboard("ap")
+      await userEvent.keyboard("{ArrowDown}")
+      await userEvent.keyboard("{Enter}")
+      expect(input).toHaveValue("Apples")
+      expect(onSelect).toHaveReturnedWith({ item: { label: "Apples", value: "Apples", lowerCasedValue: "apples", details: {} } })
+    })
+    await userEvent.clear(input)
   },
 }
--- src/components/Autocomplete/Autocomplete.svelte
+++ src/components/Autocomplete/Autocomplete.svelte
@@ -1,82 +1,60 @@
-<script>
-  import { clickOutside } from '../../events/clickOutside';
+<script lang="ts">
   import { createEventDispatcher } from 'svelte';
-  import isSvelteComponent from '../../utils/isSvelteComponent';
   import Circle from '../../assets/icons/times-circle-solid.svg.svelte';
 
   export let data = [];
-  
-  // The placeholder text for the input field
   export let query = '';
-
-  // The placeholder text for the input field
   export let placeholder = 'Platzhalter';
 
-  // The class applied to the input field.
-  export let inputClass = '';
-
-  // The class applied to the list container.
-  export let listClass = '';
-
-  // The class applied to the labels.
-  export let labelClass = '';
-
-  let input;
+  let listRef;
   const dispatch = createEventDispatcher();
+
   let shownItems = data.sort((a, b) => a.value.localeCompare(b.value)).slice(0, 250);
   let highlightedItemIndex = -1;
-  let open = false;
+  $: isActive = false;
 
-  // display max 250 items in dropdown; otherwise dropdown is lagging
-  // on weaker devices
+  // display max 250 items in dropdown
   const setShownItems = (d) => (shownItems = d.slice(0, 250));
 
-  // open dropdown if input is clicked
-  const handleClick = () => {
-    open = true;
-    input.select();
-  };
-
-  // close dropdown if clicked outside
-  const handleClickOutside = () => {
-    open = false;
-  };
-
-  // insert clicked item into search input, dispatch it as select event
-  // and close dropdown
-  // function is also used by handleKeyDown function
+  // Insert clicked item into search input,
+  // dispatch it as select event and close the dropdown
   const handleItemClick = (index) => {
-    const item = shownItems[index];
-    query = item.value;
-    dispatch('select', {
-      item
-    });
-    open = false;
-    input.blur();
+    highlightedItemIndex = index;
+    setSelectedItem(shownItems[highlightedItemIndex]);
   };
 
-  // Use dropdown with keyboard:
-  // Arrow up/down: Move trough dropdown menu
-  // Enter: Trigger selection
+  const setSelectedItem = (item) => {
+    if (!item) return;
+    query = item.value;
+    dispatch('select', { item });
+  };
+
+  // Register keyboard events
   const handleKeyDown = (e) => {
-    if (e.key === 'ArrowDown' && highlightedItemIndex <= shownItems.length - 1) {
-      highlightedItemIndex += 1;
-    } else if (e.key === 'ArrowUp' && highlightedItemIndex > -1) {
-      highlightedItemIndex === 0
-        ? (highlightedItemIndex = shownItems.length - 1)
-        : (highlightedItemIndex -= 1);
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      highlightedItemIndex =
+        highlightedItemIndex < shownItems.length - 1 ? highlightedItemIndex + 1 : 0;
+      const target = listRef.querySelector(`ul li:nth-child(${highlightedItemIndex + 1})`);
+      target.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    } else if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      highlightedItemIndex =
+        highlightedItemIndex > 0 ? highlightedItemIndex - 1 : shownItems.length - 1;
+      const target = listRef.querySelector(`ul li:nth-child(${highlightedItemIndex + 1})`);
+      target.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
     } else if (e.key === 'Enter') {
-      handleItemClick(highlightedItemIndex);
+      setSelectedItem(shownItems[highlightedItemIndex]);
+      isActive = false;
     }
   };
 
   const handleRemove = (e) => {
     query = '';
+    setShownItems(data);
     dispatch('select', {
       item: undefined
     });
-    open = false;
-    setShownItems(data);
   };
 
   // update dropdown list if input value changes
@@ -106,126 +84,139 @@
 </script>
 
 <!--
-  This component is used to display a list of suggestions for a given query. If an item is clicked or an enter key is pressed, the selected item will be returned by a custom `select` event.
-    
-  Data should be provided as array of objects of the following shape:
-  - **value** *string* the text that is displayed in the input. Has to be unique!
-  - **label** *string* | *svelte component* | *array of svelte component and props object* text, html or component displayed in the dropdown list
-  - **details** *object* extra data passed to the selection handler
-  @component
+This component is used to display a list of suggestions for a given query. If an item is clicked or the enter key is pressed, the selected item will be returned by a custom select event.
+
+Data should be provided as array of objects. Each object contains the information for a dropdown entry:
+    - **value** *string* the text that is displayed in the input. Has to be unique!
+    - **label** *string* | *svelte component* | *array of svelte component and props object* text, html or component displayed in the dropdown list
+    - **details** *object* extra data needed by selection handler
+@component
 -->
 
-<svelte:window on:keydown={handleKeyDown} />
-
-<div class="container" use:clickOutside on:clickOutside={handleClickOutside}>
+<div class="autocomplete" on:keydown={handleKeyDown}>
   <input
     type="text"
-    class={`${inputClass} ${open ? 'open' : 'close'}`}
+    {placeholder}
     autocomplete="off"
     autocorrect="off"
     autocapitalize="off"
-    {placeholder}
-    bind:this={input}
+    data-testid="autocomplete-input"
     bind:value={query}
     on:input={filterCountries}
-    on:click={handleClick}
+    on:focus={() => {
+      isActive = true;
+    }}
+    on:blur={() => {
+      window.setTimeout(() => {
+        isActive = false;
+      }, 100);
+    }}
   />
-  {#if open && shownItems.length}
-    <ul class={listClass}>
-      {#each shownItems as item, i (item.value)}
-        <li
-          class={labelClass}
-          on:mouseover={() => (highlightedItemIndex = i)}
-          on:focus={() => (highlightedItemIndex = i)}
-          on:click={() => {
+
+  <ul tabindex="-1" bind:this={listRef} class:active={isActive}>
+    {#each shownItems as item, i (item.value)}
+      <li>
+        <button
+          class={`item ${i === highlightedItemIndex ? 'active' : ''}`}
+          tabindex="-1"
+          type="button"
+          on:click={(e) => {
+            e.preventDefault();
             handleItemClick(i);
           }}
-          data-active={i === highlightedItemIndex}
         >
-          {#if isSvelteComponent(item.label)}
-            <svelte:component this={item.label} />
-          {:else if Array.isArray(item.label) && isSvelteComponent(item.label[0]) && item.label[1] instanceof Object}
-            <svelte:component this={item.label[0]} {...item.label[1]} />
-          {:else}
-            {@html item.label}
-          {/if}
-        </li>
-      {/each}
-    </ul>
-  {/if}
-  {#if query.length}
-    <div on:click={handleRemove} class="icon">
-      <Circle />
-    </div>
-  {/if}
+          {item.label}
+        </button>
+      </li>
+    {/each}
+  </ul>
+  <button type="button" on:click={handleRemove} class="clear" class:active={query.length > 0}>
+    <Circle />
+  </button>
 </div>
 
 <style lang="scss">
   @import '../../styles/scss/base.scss';
 
-  .container {
+  .autocomplete {
     position: relative;
     display: block;
-    color: black;
+    color: white;
+
     input {
+      @extend %copy;
       border: 1px solid black;
       border-radius: $border-radius-input;
       background: transparent;
-      padding: 0 0.75em;
+      padding: 0 0.5em;
       height: $input-height;
+      margin: 0;
       display: block;
-      outline-offset: 2px;
-      text-size-adjust: none;
       width: 100%;
       &:focus-visible {
-        outline: 2px solid gray;
-      }
-      &.open {
-        border-bottom-left-radius: 0;
         border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
       }
+    }
+    input[type='text'] {
+      text-size-adjust: none;
     }
 
     ul {
       position: absolute;
       top: 100%;
-      border: 1px solid gray;
+      border: 1px solid black;
       border-bottom-left-radius: $border-radius-input;
       border-bottom-right-radius: $border-radius-input;
       border-top: 0;
       left: 0;
       right: 0;
       max-height: 10rem;
-      background: white;
       overflow-y: scroll;
       z-index: 1000;
+      opacity: 0;
+      transition: 100ms;
+      pointer-events: none;
+      &:empty {
+        box-shadow: none;
+      }
+      &.active {
+        opacity: 1;
+        pointer-events: all;
+      }
 
-      li {
+      .item {
+        @extend %caption;
         padding: 0.5em;
-        border-bottom: 1px solid gray;
+        width: 100%;
+        background: transparent;
+        border: 0;
+        text-align: left;
+        border-bottom: 1px solid rgba(black, 0.1);
         cursor: pointer;
-        &[data-active='true'] {
+        &.active, &:hover {
           text-decoration: underline;
-        }
-
-        &:last-child {
-          padding-bottom: 0.3rem;
-          border-bottom: 0;
         }
       }
     }
 
-    .icon {
+    .clear {
       position: absolute;
       transform: translateY(-50%);
       top: 50%;
-      right: 0.5rem;
-      width: 1rem;
-      height: 1rem;
-
+      right: 0.75rem;
+      width: 1.2rem;
+      height: 1.2rem;
+      background: transparent;
+      border: 0;
+      color: white;
+      display: none;
+      &.active {
+        display: block;
+      }
       &:hover,
-      &:focus-visible {
-        color: red;
+      &:focus {
+        color: $orange--1;
         cursor: pointer;
       }
     }
