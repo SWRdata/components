442469297edf6bda7f4b7ac71c530a89a70b21d1
max k <hello@maxkohler.com>
2024-10-14 12:36:55 +0200

    fix: Closes #3
    * Adds Container, Card, Button, Input library components

---
+++ components/src/Card/Card.svelte
@@ -0,0 +1,19 @@
+<div class="card">
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '../styles/base.scss';
+	.card {
+		width: auto;
+		max-width: $app-max-width;
+		color: white;
+		background: $reanimation-violetblue;
+		border-radius: $border-radius-container;
+		margin: 0 1rem;
+		padding: 1.5rem;
+		@include bp($break-tablet) {
+			padding: 2.5rem;
+		}
+	}
+</style>
---
+++ components/src/Container/Container.svelte
@@ -0,0 +1,12 @@
+<div>
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '../styles/base.scss';
+	:host {
+		--swr-sans: #{$swr-sans};
+		--swr-text: #{$swr-text};
+		--orange: #{$orange};
+	}
+</style>
