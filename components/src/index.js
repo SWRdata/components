7e1cbca85fff1c171ab68c22e23fb7aca7080c00
Max Kohler <hello@maxkohler.com>
2024-10-14 14:46:37 +0200

    fix: Configure package exports

---
+++ components/src/index.js
@@ -0,0 +1,6 @@
+export { default as Card } from './Card/Card.svelte';
+export { default as Container } from './Container/Container.svelte';
+export { default as Autocomplete } from './Autocomplete/Autocomplete.svelte';
+export { default as Switcher } from './Switcher/Switcher.svelte';
+export { default as Input } from './Input/Input.svelte';
+export { default as Button } from './Button/Button.svelte';
