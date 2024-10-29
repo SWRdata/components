f82ecb57c7060219f08837a7ee720b2abf3b05ea
Simon Jockers <449739+sjockers@users.noreply.github.com>
2024-10-29 17:38:14 +0100

    Migrate stories to Svelte CSF format
    (enable Slots and other Svelte-specific features in stories)

---
+++ components/src/Select/Select.stories.svelte
@@ -0,0 +1,116 @@
+<script context="module">
+	import Select from './Select.svelte';
+	import groupedJobs from '../assets/mock_data/jobs_grouped.json';
+	import groupedThreeDigits from '../assets/mock_data/jobs_3.json';
+	import groupedFourDigits from '../assets/mock_data/jobs_4.json';
+
+	export const meta = {
+		title: 'Input Components/Svelte Select',
+		component: Select,
+		// FIXME: remove this manual documentation when autodocs are fixed
+		// TODO: finish documentation
+		parameters: {
+			docs: {
+				description: {
+					component:
+						'This component is a select input with search feature and various options such as grouped items, multi-select etc. based on https://github.com/rob-balfre/svelte-select'
+				}
+			}
+		},
+		argTypes: {
+			inputId: {
+				description: "The input field's ID",
+				control: 'text',
+				type: { name: 'string', required: false },
+				defaultValue: 'select'
+			},
+			placeholder: {
+				description: "The input field's placeholder text",
+				control: 'text',
+				type: { name: 'string', required: false },
+				defaultValue: 'Bitte auswählen'
+			}
+			// items (interface)
+			// groupHeaderSelectable
+		}
+	};
+</script>
+
+<script>
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Select {...args} />
+</Template>
+
+<Story
+	name="Simple"
+	args={{
+		items: [
+			{ value: 'chocolate', label: 'Chocolate' },
+			{ value: 'cake', label: 'Cake' },
+			{ value: 'ice-cream', label: 'Ice Cream' }
+		]
+	}}
+/>
+
+<Story
+	name="Grouped"
+	args={{
+		items: [
+			{ value: 'chocolate', label: 'Chocolate', group: 'Sweet' },
+			{ value: 'pizza', label: 'Pizza', group: 'Savory' },
+			{ value: 'cake', label: 'Cake', group: 'Sweet' },
+			{ value: 'chips', label: 'Chips', group: 'Savory' },
+			{ value: 'ice-cream', label: 'Ice Cream', group: 'Sweet' }
+		]
+	}}
+/>
+
+<Story
+	name="Jobs (3 digits)"
+	args={{
+		inputId: 'job-select',
+		placeholder: 'Ihr Beruf',
+		items: groupedThreeDigits
+	}}
+/>
+
+<Story
+	name="Jobs (3 digits)"
+	args={{
+		inputId: 'job-select',
+		placeholder: 'Ihr Beruf',
+		items: groupedThreeDigits
+	}}
+/>
+
+<Story
+	name="Jobs (4 digits)"
+	args={{
+		inputId: 'job-select',
+		placeholder: 'Ihr Beruf',
+		items: groupedFourDigits
+	}}
+/>
+
+<Story
+	name="Jobs (grouped)"
+	args={{
+		inputId: 'job-select',
+		placeholder: 'Ihr Beruf',
+		items: groupedJobs,
+		groupHeaderSelectable: false
+	}}
+/>
+
+<Story
+	name="Jobs (grouped, group header selectable)"
+	args={{
+		inputId: 'job-select',
+		placeholder: 'Ihr Beruf',
+		items: groupedJobs,
+		groupHeaderSelectable: true
+	}}
+/>
--- components/src/Select/Select.svelte
+++ components/src/Select/Select.svelte
@@ -1,50 +1,51 @@
 <script lang="ts">
-    import Select from 'svelte-select';
+	import Select from 'svelte-select';
 
-    // FUTURE: is it possible to highlight match in select options?
+	// FUTURE: is it possible to highlight match in select options?
 
-    /** FIXME:
-     * NOTE: Storybook autodocs are not working for this component 
-     * because of the Select import. Set the docs manually instead. */
+	/** FIXME:
+	 * NOTE: Storybook autodocs are not working for this component
+	 * because of the Select import. Set the docs manually instead. */
 
-    /**
-     * The interface for select options
-     */
-    interface SelectItem {
-        value: string;
-        label: string;
-        group?: string;
-    }
+	/**
+	 * The interface for select options
+	 */
+	interface SelectItem {
+		value: string;
+		label: string;
+		group?: string;
+	}
 
-    /**
-     * The input field's ID
-     */
-    export let inputId: string = 'select';
+	/**
+	 * The input field's ID
+	 */
+	export let inputId: string = 'select';
 
-    /**
-     * The input field's placeholder text
-     */
-    export let placeholder: string = 'Bitte auswählen';
+	/**
+	 * The input field's placeholder text
+	 */
+	export let placeholder: string = 'Bitte auswählen';
 
-    /**
-     * The list of select options
-     */
-    export let items: SelectItem[] = [];
+	/**
+	 * The list of select options
+	 */
+	export let items: SelectItem[] = [];
 
-    $: items, console.log(items);
+	$: items, console.log(items);
 
-    let groupBy: ((item: SelectItem) => string) | undefined;
-    $: groupBy = items.length > 0 && items.every(item => item.group)
-        ? (item: SelectItem) => item.group as string
-        : undefined;
+	let groupBy: ((item: SelectItem) => string) | undefined;
+	$: groupBy =
+		items.length > 0 && items.every((item) => item.group)
+			? (item: SelectItem) => item.group as string
+			: undefined;
 
-    /**
-     * Whether the group names should be selectable as well
-     */
-    export let groupHeaderSelectable: boolean = false;
+	/**
+	 * Whether the group names should be selectable as well
+	 */
+	export let groupHeaderSelectable: boolean = false;
 
-    let selectedItem: SelectItem | undefined;
-    // $: selectedItem: console.log("Selected item: ", selectedItem);
+	export let selectedItem: SelectItem | undefined;
+	// $: selectedItem: console.log("Selected item: ", selectedItem);
 </script>
 
 <!--
@@ -54,22 +55,22 @@ based on https://github.com/rob-balfre/svelte-select
 @component
 -->
 
-<Select 
-    items={items} 
-    groupBy={groupBy} 
-    id={inputId} 
-    placeholder={placeholder} 
-    groupHeaderSelectable={groupHeaderSelectable}
-    bind:value={selectedItem}
+<Select
+	{items}
+	{groupBy}
+	id={inputId}
+	{placeholder}
+	{groupHeaderSelectable}
+	bind:value={selectedItem}
 ></Select>
 
 <style lang="scss">
 	@import '../styles/base.scss';
 
-    // TODO: styling (font etc.)
-    // .svelte-select, .svelte-select-list {
-    //     @extend %copy;
-    // }
+	// TODO: styling (font etc.)
+	// .svelte-select, .svelte-select-list {
+	//     @extend %copy;
+	// }
 	// 	--border: 3px solid blue;
 	// 	--border-radius: 10px;
 	// 	--placeholder-color: blue;
