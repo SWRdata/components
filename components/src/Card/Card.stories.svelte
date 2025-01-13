27807c1f91876b1233a623a59e3560d575f41b76
Simon Jockers <449739+sjockers@users.noreply.github.com>
2025-01-13 12:00:15 +0100

    Add basic card story

---
+++ components/src/Card/Card.stories.svelte
@@ -0,0 +1,43 @@
+<script context="module">
+	import Card from './Card.svelte';
+
+	export const meta = {
+		title: 'Input Components/Card',
+		component: Card
+	};
+</script>
+
+<script>
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import {
+		userEvent,
+		within,
+		expect,
+		getByTestId,
+		getAllByLabelText,
+		getByText
+	} from '@storybook/test';
+	import { hasContext } from 'svelte';
+	import Input from '../Input/Input.svelte';
+
+	let component;
+</script>
+
+<Story name="Basic card">
+	<Card>
+		<h1>Any content</h1>
+		<p>can be added to a basic <code>Card</code> component.</p>
+	</Card>
+</Story>
+
+<Template let:args>
+	<Card {...args} bind:this={component}><slot /></Card>
+</Template>
+
+<Story
+	name="Simple"
+	args={{
+		inputId: 'select',
+		clearable: true
+	}}
+/>
