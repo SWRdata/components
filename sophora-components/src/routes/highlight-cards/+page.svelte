e2cee24a236905406c83f7a799040f845d514e83
Simon Jockers <449739+sjockers@users.noreply.github.com>
2025-01-16 17:48:45 +0100

    feat: Add sophora-components setup (#19)
    * Add basic setup for building and deploying sophora-components
    
    * Add demo component (HighlightCards)
    
    * Enforce prerender for static builds
    
    * rename sophora-components deployment script

---
+++ sophora-components/src/routes/highlight-cards/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import HighlightCard from '@components/HighlightCard';
+</script>
+
+<div class="highlight-cards">
+	<HighlightCard
+		topline="Im aktuellen Jahr wurden bisher"
+		highlight="375.000"
+		highlightSecondary="€"
+		subline="an Parteien gespendet."
+	/>
+	<HighlightCard
+		topline="Die letzte Großspende liegt bei"
+		highlight="37.500"
+		highlightSecondary="€"
+		subline="Der Betrag ging am 7. Januar von Dr. Kim Musterperson ein."
+	/>
+</div>
+
+<style>
+	.highlight-cards {
+		display: flex;
+		margin-top: 2rem;
+	}
+
+	:global(.highlight-cards > *) {
+		flex: 1;
+	}
+</style>
---
+++ sophora-components/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	preprocess: [vitePreprocess()]
+};
+
+export default config;
