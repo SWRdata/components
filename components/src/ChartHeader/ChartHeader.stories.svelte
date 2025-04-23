e5f664938d48f736913829fda945a62efc606cc3
max k <hello@maxkohler.com>
2025-04-23 12:53:46 +0200

    feat: 40
    * Set ID attribute on ChartHeader using slugified title prop
    * Add test

--- components/src/ChartHeader/ChartHeader.stories.svelte
+++ components/src/ChartHeader/ChartHeader.stories.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import DesignTokens from '../DesignTokens/DesignTokens.svelte';
-
+	import { expect } from '@storybook/test';
 	import ChartHeader from './ChartHeader.svelte';
 
 	const { Story } = defineMeta({
@@ -10,7 +10,15 @@
 	});
 </script>
 
-<Story name="Default">
+<Story
+	name="Default"
+	play={async ({ step }) => {
+		await step('Container has ID attribute derived from title prop', async () => {
+			const containerEl = document.querySelector('#mehr-uber-60-jahrige-in-allen-berufen');
+			expect(containerEl).toBeTruthy();
+		});
+	}}
+>
 	<DesignTokens>
 		<ChartHeader
 			title="Mehr über-60-Jährige in allen Berufen"
