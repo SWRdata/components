66ec77df5fe67152a96214d20da0aa470e3bb4bc
Max Kohler <hello@maxkohler.com>
2025-04-23 11:50:43 +0200

    Fix types

--- components/src/utils/slugify.ts
+++ components/src/utils/slugify.ts
@@ -1,6 +1,6 @@
 // Source: https://svelte.dev/playground/b130be5e485441a1842ae97e4ce4f244?version=5.20.0
 
-export default function slugify(str: String) {
+export default function slugify(str: string): string {
   return str
     .replace(/\s+/g, '-')
     .replace(/-+/g, '-')
