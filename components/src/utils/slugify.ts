f4d48ae3c7afa71d24719ae5bda91cdaf2b25f4f
Max Kohler <hello@maxkohler.com>
2025-04-23 11:48:57 +0200

    Add source

--- components/src/utils/slugify.ts
+++ components/src/utils/slugify.ts
@@ -1,6 +1,8 @@
+// Source: https://svelte.dev/playground/b130be5e485441a1842ae97e4ce4f244?version=5.20.0
+
 export default function slugify(str: String) {
   return str
-    .replace(/\s+/g, '-') 
+    .replace(/\s+/g, '-')
     .replace(/-+/g, '-')
     .trim()
     .normalize('NFKD')
