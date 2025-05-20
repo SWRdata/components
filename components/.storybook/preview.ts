2cf80a40f943e7ca486bd828a39f55e1a31892be
Max Kohler <hello@maxkohler.com>
2025-05-20 11:29:44 +0200

    Roll back msw plugin

--- components/.storybook/preview.ts
+++ components/.storybook/preview.ts
@@ -1,52 +1,41 @@
-import type { Preview } from "@storybook/sveltekit";
+import type { Preview } from '@storybook/sveltekit';
 
 const preview: Preview = {
-  parameters: {
-    options: {
-      storySort: {
-        order:
-          [
-            'About',
-            'Design Tokens',
-            "Typography",
-            [
-              "Headline",
-              "Copy",
-              "Caption"
-            ],
-            "Display",
-            "Chart",
-            [
-              "ChartHeader"
-            ],
-            "Map",
-            [
-              "Map",
-              "MapStyle",
-              "MapSource",
-              "VectorTileSource",
-              "VectorLayer",
-              "Tooltip",
-              "Control",
-              [
-                "MapControl",
-                "ScaleControl",
-                "NavigationControl"
-              ]
-            ],
-            "Form",
-            "Deprecated"
-          ]
-      },
-    },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-  tags: ["autodocs"],
+	parameters: {
+		options: {
+			storySort: {
+				order: [
+					'About',
+					'Design Tokens',
+					'Typography',
+					['Headline', 'Copy', 'Caption'],
+					'Display',
+					'Chart',
+					['ChartHeader'],
+					'Map',
+					[
+						'Map',
+						'MapStyle',
+						'MapSource',
+						'VectorTileSource',
+						'VectorLayer',
+						'Tooltip',
+						'Control',
+						['MapControl', 'ScaleControl', 'NavigationControl']
+					],
+					'Form',
+					'Deprecated'
+				]
+			}
+		},
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	},
+	tags: ['autodocs']
 };
 
 export default preview;
