d6afc50456f34e7631d4cc2d8f6d215dad33d99b
Max Kohler <hello@maxkohler.com>
2025-05-25 11:59:29 +0200

    Block out improved release config

--- components/src/utils/getEmbedContext.js
+++ components/src/utils/getEmbedContext.js
@@ -5,24 +5,23 @@
  */
 
 const getEmbedContext = (target) => {
-  let url;
-  if (
-    import.meta.env.DEV ||
-    window.location.origin === 'https://d.swr.de' ||
-    window.location.origin === 'https://static.datenhub.net' ||
-    window.location.href.includes('apidata.googleusercontent.com') ||
-    window.location.href.includes('storage.googleapis.com')
-  ) {
-    url = window.location.href;
-  } else {
-    const parent = target.parentNode || target.parentNode.parentNode;
-    url = parent.dataset.url;
-  }
-  const data = {};
-  for (const [key, value] of new URL(url).searchParams) {
-    data[key] = value;
-  }
-  return data;
+	let url;
+	if (
+		window.location.origin === 'https://d.swr.de' ||
+		window.location.origin === 'https://static.datenhub.net' ||
+		window.location.href.includes('apidata.googleusercontent.com') ||
+		window.location.href.includes('storage.googleapis.com')
+	) {
+		url = window.location.href;
+	} else {
+		const parent = target.parentNode || target.parentNode.parentNode;
+		url = parent.dataset.url;
+	}
+	const data = {};
+	for (const [key, value] of new URL(url).searchParams) {
+		data[key] = value;
+	}
+	return data;
 };
 
 export default getEmbedContext;
