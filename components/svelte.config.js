68cf7acabfd6918318c2375bf767350a0a4047d7
Max Kohler <hello@maxkohler.com>
2025-06-02 18:57:38 +0200

    chore(docs): Add comment

--- components/svelte.config.js
+++ components/svelte.config.js
@@ -5,12 +5,11 @@ import { sveltePreprocess } from 'svelte-preprocess';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
+
+	// We can't use vitePreprocess() here, see: https://github.com/sveltejs/kit/issues/13122
 	preprocess: sveltePreprocess({ scss: true }),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
