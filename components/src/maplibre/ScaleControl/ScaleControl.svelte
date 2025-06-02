80998458974448016999b087d787636d08ea8683
max k <hello@maxkohler.com>
2025-06-02 16:30:42 +0200

    fix(Maplibre): Revert to in-component styles (#82)
    * Revert to in-component styles
    
    * Drop unused svelte-preprocess

--- components/src/maplibre/ScaleControl/ScaleControl.svelte
+++ components/src/maplibre/ScaleControl/ScaleControl.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import maplibre, { type ControlPosition, type Unit } from 'maplibre-gl';
 	import MapControl from '../MapControl/MapControl.svelte';
-	import './ScaleControl.css';
 
 	interface ScaleControlProps {
 		maxWidth?: number | undefined;
@@ -12,3 +11,13 @@
 </script>
 
 <MapControl control={new maplibre.ScaleControl({ maxWidth, unit })} {position} />
+
+<style lang="scss">
+	.maplibregl-ctrl-scale {
+		color: inherit;
+		border-bottom: 1px solid currentColor;
+		font-weight: 400;
+		font-size: var(--fs-small-3);
+		font-family: var(--swr-text);
+	}
+</style>
