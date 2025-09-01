6a862d2b07efcd898015a202053f3ce884609fb3
Max Kohler <hello@maxkohler.com>
2025-09-01 17:16:11 +0200

    Add showLogo prop, tests

--- components/src/ChartFooter/ChartFooter.stories.svelte
+++ components/src/ChartFooter/ChartFooter.stories.svelte
@@ -1,6 +1,7 @@
 <script context="module">
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import DesignTokens from '../DesignTokens/DesignTokens.svelte';
+	import { expect } from 'storybook/test';
 
 	import ChartFooter from './ChartFooter.svelte';
 	import Middot from '../Middot/Middot.svelte';
@@ -25,6 +26,26 @@
 <Story name="One-up Centered" asChild>
 	<DesignTokens>
 		<ChartFooter layout="one-up" align="center">
+			Daten: <a href="#1">Zensus 2022</a>
+			(Durchschnittsmieten und Einwohnerzahlen),
+			<a href="#1">OpenStreetMap</a> (Kartenmaterial) <Middot /> In unserer Darstellung wurde das Zensusgitter
+			auf bewohnte Gebiete begrenzt.
+		</ChartFooter>
+	</DesignTokens>
+</Story>
+
+<Story
+	name="One-up Centered, no Logo"
+	asChild
+	play={async ({ step }) => {
+		await step('Does not render the logo', async () => {
+			const el = document.querySelector('.container > svg');
+			expect(el).toBeFalsy();
+		});
+	}}
+>
+	<DesignTokens>
+		<ChartFooter layout="one-up" align="center" showLogo={false}>
 			Daten: <a href="#1">Zensus 2022</a>
 			(Durchschnittsmieten und Einwohnerzahlen),
 			<a href="#1">OpenStreetMap</a> (Kartenmaterial) <Middot /> In unserer Darstellung wurde das Zensusgitter
