e5801a6a8d4e83f677da00b367e10bb5d6297f7c
max k <hello@maxkohler.com>
2025-10-06 07:53:27 +0200

    feat(maplibre): Add <InspectControl/> (#195)

--- components/src/maplibre/GeoJSONSource/GeoJSONSource.stories.svelte
+++ components/src/maplibre/GeoJSONSource/GeoJSONSource.stories.svelte
@@ -4,6 +4,7 @@
 	import GeoJSONSource from './GeoJSONSource.svelte';
 	import VectorLayer from '../VectorLayer/VectorLayer.svelte';
 	import DesignTokens from '../../DesignTokens/DesignTokens.svelte';
+	import InspectControl from '../InspectControl/InspectControl.svelte';
 	import AttributionControl from '../AttributionControl/AttributionControl.svelte';
 
 	import { SWRDataLabLight } from '../MapStyle';
@@ -49,6 +50,7 @@
 						'line-opacity': 1
 					}}
 				/>
+				<InspectControl />
 				<AttributionControl />
 			</Map>
 		</div>
---
+++ components/src/maplibre/InspectControl/InspectControl.stories.svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import InspectControl from './InspectControl.svelte';
+	import DesignTokens from '../../DesignTokens/DesignTokens.svelte';
+	import { SWRDataLabLight } from '../MapStyle';
+	import Map from '../Map/Map.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Maplibre/Control/InspectControl',
+		component: InspectControl
+	});
+</script>
+
+<Story asChild name="Default">
+	<DesignTokens theme="light">
+		<div class="container">
+			<Map style={SWRDataLabLight()} initialLocation={{ lat: 51, lng: 10, zoom: 20 }}>
+				<InspectControl />
+			</Map>
+		</div>
+	</DesignTokens>
+</Story>
+
+<style>
+	.container {
+		width: 500px;
+		height: 200px;
+	}
+</style>
