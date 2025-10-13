b4a5bfdbd2cb0021210665abc6a2dba8a6e5fc8c
Max Kohler <hello@maxkohler.com>
2025-10-13 11:54:18 +0200

    Add reproduction

--- components/src/maplibre/ArrowSource/ArrowSource.stories.svelte
+++ components/src/maplibre/ArrowSource/ArrowSource.stories.svelte
@@ -3,7 +3,6 @@
 	import Map from '../Map/Map.svelte';
 	import ArrowSource from './ArrowSource.svelte';
 	import VectorLayer from '../VectorLayer/VectorLayer.svelte';
-	import DesignTokens from '../../DesignTokens/DesignTokens.svelte';
 	import InspectControl from '../InspectControl/InspectControl.svelte';
 	import AttributionControl from '../AttributionControl/AttributionControl.svelte';
 
@@ -17,76 +16,132 @@
 </script>
 
 <Story asChild name="Default">
-	<DesignTokens theme="light">
-		<div class="container">
-			<Map
-				showDebug={true}
-				style={SWRDataLabLight()}
-				initialLocation={{
-					lng: -84.783,
-					lat: 15.623,
-					zoom: 3.39,
-					pitch: 0
+	<div class="container">
+		<Map
+			showDebug={true}
+			style={SWRDataLabLight()}
+			initialLocation={{
+				lng: -84.783,
+				lat: 15.623,
+				zoom: 3.39,
+				pitch: 0
+			}}
+		>
+			<ArrowSource
+				id="demo"
+				attribution="Demo attribution"
+				arrows={[
+					{
+						width: 10,
+						a: [-80.1, 11.3],
+						b: [-84.783, 15.6],
+						c: [-81, 14.6]
+					},
+					{
+						width: 15,
+						a: [-71.1, 11.3],
+						b: [-75.783, 15.6],
+						c: [-75, 12.6]
+					}
+				]}
+			/>
+			<InspectControl />
+			<AttributionControl />
+			<VectorLayer
+				sourceId="demo"
+				id="arrow-tails"
+				filter={['==', 'kind', 'arrow-tail']}
+				type="line"
+				paint={{
+					'line-gradient': [
+						'interpolate',
+						['linear'],
+						['line-progress'],
+						0,
+						'transparent',
+						0.4,
+						tokens.shades.red.base
+					],
+					'line-width': ['get', 'width']
 				}}
-			>
-				<ArrowSource
-					id="demo"
-					attribution="Demo attribution"
-					arrows={[
-						{
-							width: 10,
-							a: [-80.1, 11.3],
-							b: [-84.783, 15.6],
-							c: [-81, 14.6]
-						},
-						{
-							width: 15,
-							a: [-71.1, 11.3],
-							b: [-75.783, 15.6],
-							c: [-75, 12.6]
-						}
-					]}
-				/>
-				<InspectControl />
-				<AttributionControl />
-				<VectorLayer
-					sourceId="demo"
-					id="arrow-tails"
-					filter={['==', 'kind', 'arrow-tail']}
-					type="line"
-					paint={{
-						'line-gradient': [
-							'interpolate',
-							['linear'],
-							['line-progress'],
-							0,
-							'transparent',
-							0.4,
-							tokens.shades.red.base
-						],
-						'line-width': ['get', 'width']
-					}}
-				/>
-				<VectorLayer
-					sourceId="demo"
-					id="arrow-heads"
-					filter={['==', 'kind', 'arrow-head']}
-					type="symbol"
-					paint={{
-						'icon-color': tokens.shades.red.base
-					}}
-					layout={{
-						'icon-image': 'arrow-head',
-						'icon-anchor': 'top',
-						'icon-offset': [0, -2],
-						'icon-rotate': ['get', 'angle'],
-						'icon-overlap': 'always',
-						'icon-size': ['get', 'size']
-					}}
-				/>
-			</Map>
-		</div>
-	</DesignTokens>
+			/>
+			<VectorLayer
+				sourceId="demo"
+				id="arrow-heads"
+				filter={['==', 'kind', 'arrow-head']}
+				type="symbol"
+				paint={{
+					'icon-color': tokens.shades.red.base
+				}}
+				layout={{
+					'icon-image': 'arrow-head',
+					'icon-anchor': 'top',
+					'icon-offset': [0, -2],
+					'icon-rotate': ['get', 'angle'],
+					'icon-overlap': 'always',
+					'icon-size': ['get', 'size']
+				}}
+			/>
+		</Map>
+	</div>
+</Story>
+
+<Story asChild name="fix/206">
+	<div class="container">
+		<Map
+			showDebug={true}
+			style={SWRDataLabLight()}
+			initialLocation={{
+				lng: 7.72,
+				lat: 47.59,
+				zoom: 10.9
+			}}
+		>
+			<ArrowSource
+				id="arrows"
+				attribution=""
+				arrows={[{ a: [7.78, 47.55917], b: [7.667, 47.595], c: [7.69, 47.55], width: 30 }]}
+			/>
+
+			<InspectControl />
+			<AttributionControl />
+			<VectorLayer
+				sourceId="arrows"
+				id="arrow-tails"
+				filter={['==', 'kind', 'arrow-tail']}
+				type="line"
+				paint={{
+					'line-gradient': [
+						'interpolate',
+						['linear'],
+						['line-progress'],
+						0,
+						'transparent',
+						0.4,
+						tokens.shades.red.base
+					],
+					'line-width': ['get', 'width']
+				}}
+			/>
+			<VectorLayer
+				sourceId="arrows"
+				id="arrow-heads"
+				filter={['==', 'kind', 'arrow-head']}
+				type="symbol"
+				paint={{
+					'icon-color': tokens.shades.red.base
+				}}
+				layout={{
+					'icon-image': 'arrow-head',
+					'icon-anchor': 'top',
+					'icon-offset': [0, -2],
+					'icon-rotate': ['get', 'angle'],
+					'icon-overlap': 'always',
+					'icon-size': ['get', 'size']
+				}}
+			/>
+		</Map>
+	</div>
 </Story>
 
 <style>
