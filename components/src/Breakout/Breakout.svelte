7c2abfe6bbe93fe173f9ff26f3a7b55a5e83dd54
Max Kohler <hello@maxkohler.com>
2025-10-19 12:54:13 +0200

    Add files

---
+++ components/src/Breakout/Breakout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface CopyProps {
+		children?: Snippet;
+	}
+	let { children }: CopyProps = $props();
+</script>
+
+<div class="container">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style lang="scss">
+</style>
