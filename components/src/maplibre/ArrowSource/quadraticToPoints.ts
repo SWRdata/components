f30450a5572c7b1954298240b1b92c48a0f5b475
Max Kohler <hello@maxkohler.com>
2025-11-04 13:44:42 +0100

    Cleanup

--- components/src/maplibre/ArrowSource/quadraticToPoints.ts
+++ components/src/maplibre/ArrowSource/quadraticToPoints.ts
@@ -1,53 +1,43 @@
-/**
-Given a quadratic bezier defined by points a, b and c,
-returns a series of n points on the curve
-
-The quadratic bezier is: B(t) = (1-t)[(1-t)P0 + tP1] + t[(1-t)P1+tP2]
-Source: https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Quadratic_B%C3%A9zier_curves
-
-Below is a naive implementation but good enough for now. For a better approach see:
-https://bit-101.com/blog/posts/2024-09-29/evenly-placed-points-on-bezier-curves
-*/
-
 import type { V2 } from './types';
 
+/**
+ * Returns the euclidian distance between two points
+ */
 const distance = (a: V2, b: V2) => {
 	return Math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2);
 };
 
-const norm = (v: V2) => {
-	const d = distance([0, 0], v);
-	return [v[0] * d, v[1] * d];
-};
-
-const findSegment = (segments: number[], v: number) => {
-	let low = 0;
-	let high = segments.length;
+/**
+ * Given a sorted array arr and a value, returns the index i of arr
+ * such that arr[i] < value && arr[i + 1] > value
+ */
+const getSortedIndex = (arr: number[], v: number) => {
+	let low = 0,
+		high = arr.length;
 	while (low < high) {
 		let mid = (low + high) >>> 1; // === Math.round((low + high) * .5) but faster
-		if (segments[mid] < v) low = mid + 1;
+		if (arr[mid] < v) low = mid + 1;
 		else high = mid;
 	}
 	return low;
 };
 
+/**
+ * Given a polyline, returns n evenly-spaced points on the polyline
+ */
 const interpolatePolyline = (points: V2[], n: number) => {
-	let res: V2[] = [];
-	let segments: number[] = [0];
+	let res: V2[] = [],
+		totalLength: number = 0,
+		segments: number[] = [0];
 
-	let totalLength = 0;
 	for (let i = 0; i < points.length - 2; i++) {
 		totalLength += distance(points[i], points[i + 1]);
 		segments.push(totalLength);
 	}
 
-	console.log(segments);
-	console.log({ points, totalLength });
-
 	for (let i = 0; i < n; i++) {
 		const d: number = totalLength * (i / n);
-		const si: number = findSegment(segments, d);
-		console.log({ d, si });
+		const si: number = getSortedIndex(segments, d);
 		const p0 = points[si];
 		const p1 = points[si + 1];
 
@@ -60,9 +50,19 @@ const interpolatePolyline = (points: V2[], n: number) => {
 	return res;
 };
 
+/**
+Given a quadratic bezier defined by points a, b and c,
+returns a series of n points on the curve
+
+See:
+- https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Quadratic_B%C3%A9zier_curves
+- https://bit-101.com/blog/posts/2024-09-29/evenly-placed-points-on-bezier-curves
+*/
+
 const quadraticToPoints = (a: V2, b: V2, c: V2, n = 30) => {
 	let points: V2[] = [];
 
+	// 1. Interpolate the quadratic bezier function to obtain an uneven polyline
 	for (let i = 0; i < n; i++) {
 		const t = i / n;
 		points.push([
@@ -71,6 +71,7 @@ const quadraticToPoints = (a: V2, b: V2, c: V2, n = 30) => {
 		]);
 	}
 
+	// 2. Interpolate the polyline from (1) to get evenly-spaced points
 	points = interpolatePolyline([...points, b], n);
 
 	return points;
