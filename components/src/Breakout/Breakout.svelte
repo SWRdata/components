6793b42190d0126fcbd0b4f467a05bf56f210d16
max k <hello@maxkohler.com>
2025-12-30 11:04:38 +0100

    fix(Breakout, DevContainer): Adjust to updated SWR.de layout (#273)
    * Adjust DevContainer at top breakpoint
    
    * Breakout/medium: Drop top breakpoint
    
    * Apply change to large, bleed breakouts

--- components/src/Breakout/Breakout.svelte
+++ components/src/Breakout/Breakout.svelte
@@ -40,10 +40,6 @@
 			width: calc(var(--column-width) * 10 + var(--column-gap) * 9);
 			margin-left: calc((var(--column-width) * -1) + (var(--column-gap)) * -1);
 		}
-		@media (min-width: 1440px) {
-			width: calc(var(--column-width) * 8 + var(--column-gap) * 7);
-			margin-left: calc((var(--column-width) * -1) + (var(--column-gap)) * -1);
-		}
 	}
 	.large {
 		@media (min-width: 640px) {
@@ -53,10 +49,6 @@
 		@media (min-width: 1024px) {
 			width: calc(var(--column-width) * 12 + var(--column-gap) * 11);
 			margin-left: calc((var(--column-width) * -2 + var(--column-gap) * -2));
-		}
-		@media (min-width: 1440px) {
-			width: calc(var(--column-width) * 10 + var(--column-gap) * 11);
-			margin-left: calc((var(--column-width) * -2 + var(--column-gap) * -3));
 		}
 	}
 	.bleed {
@@ -71,9 +63,6 @@
 		}
 		@media (min-width: 1024px) {
 			margin-left: calc(var(--column-width) * -2 + (var(--column-gap)) * -2 - var(--margin) * 1);
-		}
-		@media (min-width: 1440px) {
-			margin-left: calc(var(--column-width) * -3 + (var(--column-gap)) * -3 - var(--margin) * 1);
 		}
 	}
 </style>
