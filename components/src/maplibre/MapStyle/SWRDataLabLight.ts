b138a8e64e4a87478f86feb0db2b41a9b53ac8ec
Max Kohler <hello@maxkohler.com>
2026-01-22 10:26:37 +0100

    Implement feature

--- components/src/maplibre/MapStyle/SWRDataLabLight.ts
+++ components/src/maplibre/MapStyle/SWRDataLabLight.ts
@@ -164,7 +164,7 @@ const style: styleFunction = (opts) => {
 			...(options.places?.showLabels ? placeLabels : []),
 
 			// 11. Admin boundary labels
-			...boundaryLabels
+			...(options.admin?.showLabels ? boundaryLabels : [])
 		]
 	};
 };
