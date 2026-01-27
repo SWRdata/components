6bbe15720de4b6d35094e83d8f0bc80ed02e1b0d
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-01-27 20:01:43 +0100

    Fix issues with chart dimensions, use `fixedHeight` property when set

--- sophora-components/src/routes/datawrapper-switcher/+page.svelte
+++ sophora-components/src/routes/datawrapper-switcher/+page.svelte
@@ -3,6 +3,7 @@
 	import getDataFromUrl from '$lib/utils/getDataFromUrl';
 	import Switcher from '@components/Switcher';
 	import { DesignTokens } from '@components/DesignTokens';
+	import DevContainer from '@components/DevContainer';
 
 	import { onMount } from 'svelte';
 
@@ -17,36 +18,49 @@
 		const entries = getDataFromUrl(root);
 		labels = entries.labels.split(',') || [];
 		ids = entries.ids.split(',') || [];
-
-		// TODO: Implement these properties
 		fixedHeight = entries.fixedHeight || null;
+
+		// TODO: Implement active color property
 		activeColor = entries.activeColor || null;
 	});
 </script>
 
-<DesignTokens theme="light">
-	<Switcher
-		options={labels}
-		size="small"
-		{activeIndex}
-		value={labels[activeIndex]}
-		onchange={({ currentTarget }) => {
-			activeIndex = labels.indexOf(currentTarget.value);
-		}}
-	/>
+<DevContainer paragraphsAbove={1} paragraphsBelow={1} showPlayer={false} showArticleHeader={false}>
+	<DesignTokens theme="light">
+		<Switcher
+			options={labels}
+			size="small"
+			{activeIndex}
+			value={labels[activeIndex]}
+			onchange={({ currentTarget }) => {
+				activeIndex = labels.indexOf(currentTarget.value);
+			}}
+		/>
 
-	{#each ids as id, index}
-		<div class="datawrapper-chart" class:datawrapper-chart-active={index === activeIndex}>
-			<script
-				type="text/javascript"
-				src={`https://datawrapper.dwcdn.net/${id}/embed.js`}
-				charset="utf-8"
-			></script>
+		<div class="datawrapper-chart-container">
+			{#each ids as id, index}
+				<div
+					class="datawrapper-chart"
+					class:datawrapper-chart-active={index === activeIndex}
+					style:height={fixedHeight ? fixedHeight + 'px' : 'auto'}
+				>
+					<script
+						type="text/javascript"
+						src={`https://datawrapper.dwcdn.net/${id}/embed.js`}
+						charset="utf-8"
+					></script>
+				</div>
+			{/each}
 		</div>
-	{/each}
-</DesignTokens>
+	</DesignTokens>
+</DevContainer>
 
 <style>
+	.datawrapper-chart-container {
+		position: relative;
+		width: 100%;
+	}
+
 	.datawrapper-chart {
 		margin-top: 1rem;
 		position: absolute;
