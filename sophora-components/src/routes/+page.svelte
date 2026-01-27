05cdde5b0d42c9c0204cf3be34e49b676d70897b
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-01-27 20:02:38 +0100

    Add ChartList with component demo

--- sophora-components/src/routes/+page.svelte
+++ sophora-components/src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
+	import { ChartList, DesignTokens } from '@swr-data-lab/components';
+	const charts = [
+		{ title: 'Highlight Cards', slug: 'highlight-cards' },
+		{
+			title: 'Datawrapper Switcher',
+			slug: 'datawrapper-switcher?ids=oKR1L,bySDe,4r4Vi&labels=Super,E10,Diesel&fixedHeight=450&activeColor=%2305556a'
+		}
+	];
 </script>
 
-<h1>Sophora Components</h1>
-<ul>
-	<li>
-		<a href="/highlight-cards">Highlight Cards</a>
-	</li>
-</ul>
-
-<style>
-	h1 {
-		font-style: var(--swr-sans);
-	}
-</style>
+<DesignTokens>
+	<ChartList
+		baseUrl="https://static.datenhub.net/apps/sophora-components/main"
+		project="Client-side components for Sophora"
+		{charts}
+	/>
+</DesignTokens>
