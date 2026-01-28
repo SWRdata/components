673c418ced88947e90bbad369de00f75318c5dea
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-01-28 10:33:46 +0100

    Extract component into separate file, remove DevContainer for deployment

--- sophora-components/src/routes/datawrapper-switcher/+page.svelte
+++ sophora-components/src/routes/datawrapper-switcher/+page.svelte
@@ -1,77 +1,21 @@
 <script>
-	import { deserialize } from '$app/forms';
-	import getDataFromUrl from '$lib/utils/getDataFromUrl';
-	import Switcher from '@components/Switcher';
-	import { DesignTokens } from '@components/DesignTokens';
 	import DevContainer from '@components/DevContainer';
-
-	import { onMount } from 'svelte';
-
-	let root;
-	let labels = [];
-	let ids = [];
-	let fixedHeight = null;
-	let activeColor = null;
-	let activeIndex = 0;
-
-	onMount(() => {
-		const entries = getDataFromUrl(root);
-		labels = entries.labels.split(',') || [];
-		ids = entries.ids.split(',') || [];
-		fixedHeight = entries.fixedHeight || null;
-
-		// TODO: Implement active color property
-		activeColor = entries.activeColor || null;
-	});
+	import DatawrapperSwitcher from './DatawrapperSwitcher.svelte';
+	import { dev } from '$app/environment';
 </script>
 
-<DevContainer paragraphsAbove={1} paragraphsBelow={1} showPlayer={false} showArticleHeader={false}>
-	<DesignTokens theme="light">
-		<Switcher
-			options={labels}
-			size="small"
-			{activeIndex}
-			value={labels[activeIndex]}
-			onchange={({ currentTarget }) => {
-				activeIndex = labels.indexOf(currentTarget.value);
-			}}
-		/>
-
-		<div class="datawrapper-chart-container">
-			{#each ids as id, index}
-				<div
-					class="datawrapper-chart"
-					class:datawrapper-chart-active={index === activeIndex}
-					style:height={fixedHeight ? fixedHeight + 'px' : 'auto'}
-				>
-					<script
-						type="text/javascript"
-						src={`https://datawrapper.dwcdn.net/${id}/embed.js`}
-						charset="utf-8"
-					></script>
-				</div>
-			{/each}
-		</div>
-	</DesignTokens>
-</DevContainer>
+{#if dev}
+	<DevContainer
+		paragraphsAbove={1}
+		paragraphsBelow={1}
+		showPlayer={false}
+		showArticleHeader={false}
+	>
+		<DatawrapperSwitcher />
+	</DevContainer>
+{:else}
+	<DatawrapperSwitcher />
+{/if}
 
 <style>
-	.datawrapper-chart-container {
-		position: relative;
-		width: 100%;
-	}
-
-	.datawrapper-chart {
-		margin-top: 1rem;
-		position: absolute;
-		opacity: 0;
-		z-index: -1;
-		width: 100%;
-	}
-
-	.datawrapper-chart-active {
-		position: relative;
-		opacity: 1;
-		z-index: 1;
-	}
 </style>
---
+++ sophora-components/src/routes/datawrapper-switcher/DatawrapperSwitcher.svelte
@@ -0,0 +1,72 @@
+<script>
+	import getDataFromUrl from '$lib/utils/getDataFromUrl';
+	import Switcher from '@components/Switcher';
+	import { DesignTokens } from '@components/DesignTokens';
+	import { onMount } from 'svelte';
+
+	let activeIndex = 0;
+	let root;
+	let labels = [];
+	let ids = [];
+	let fixedHeight = null;
+	let activeColor = null;
+
+	onMount(() => {
+		const entries = getDataFromUrl(root);
+		labels = entries.labels.split(',') || [];
+		ids = entries.ids.split(',') || [];
+		fixedHeight = entries.fixedHeight || null;
+
+		// TODO: Implement active color property
+		activeColor = entries.activeColor || null;
+	});
+</script>
+
+<DesignTokens theme="light">
+	<Switcher
+		options={labels}
+		size="small"
+		{activeIndex}
+		value={labels[activeIndex]}
+		onchange={({ currentTarget }) => {
+			activeIndex = labels.indexOf(currentTarget.value);
+		}}
+	/>
+
+	<div class="datawrapper-chart-container">
+		{#each ids as id, index}
+			<div
+				class="datawrapper-chart"
+				class:datawrapper-chart-active={index === activeIndex}
+				style:height={fixedHeight ? fixedHeight + 'px' : 'auto'}
+			>
+				<script
+					type="text/javascript"
+					src={`https://datawrapper.dwcdn.net/${id}/embed.js`}
+					charset="utf-8"
+				></script>
+			</div>
+		{/each}
+	</div>
+</DesignTokens>
+
+<style>
+	.datawrapper-chart-container {
+		position: relative;
+		width: 100%;
+	}
+
+	.datawrapper-chart {
+		margin-top: 1rem;
+		position: absolute;
+		opacity: 0;
+		z-index: -1;
+		width: 100%;
+	}
+
+	.datawrapper-chart-active {
+		position: relative;
+		opacity: 1;
+		z-index: 1;
+	}
+</style>
