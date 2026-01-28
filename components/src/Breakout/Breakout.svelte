633296c4300484eb27b4ded77383ab9f1ffe7189
max k <hello@maxkohler.com>
2026-01-28 12:13:19 +0100

    feat(Breakout): Add small layout (#294)

--- components/src/Breakout/Breakout.svelte
+++ components/src/Breakout/Breakout.svelte
@@ -2,7 +2,7 @@
 	import type { Snippet } from 'svelte';
 
 	interface BreakoutProps {
-		layout: 'medium' | 'large' | 'bleed';
+		layout: 'small' | 'medium' | 'large' | 'bleed';
 		children?: Snippet;
 	}
 	let { layout = 'medium', children }: BreakoutProps = $props();
@@ -32,6 +32,19 @@
 		@media (min-width: 1440px) {
 			--grid-width: calc(Min(1312px, 100vw));
 			--margin: calc((100vw - var(--grid-width)) * 0.5);
+		}
+	}
+
+	.small {
+		width: calc(var(--column-width) * 10 + var(--column-gap) * 8);
+		margin-left: calc((var(--column-width) * 1) + (var(--column-gap)) * 1);
+		@media (min-width: 640px) {
+			width: calc(var(--column-width) * 8 + var(--column-gap) * 6);
+			margin-left: calc((var(--column-width) * 1) + (var(--column-gap)) * 1);
+		}
+		@media (min-width: 1024px) {
+			width: calc(var(--column-width) * 6 + var(--column-gap) * 5);
+			margin-left: calc((var(--column-width) * 1) + (var(--column-gap)) * 1);
 		}
 	}
 
