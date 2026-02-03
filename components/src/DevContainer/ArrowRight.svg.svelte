515f249b9f96a776d4bfd356d0dd2440495bba91
Stephanie Jauss <37771063+stephanie-jauss@users.noreply.github.com>
2026-02-03 18:35:28 +0100

    feat(DevContainer, Breakout, SwrHeader): Match SWR.de layout (#307)
    * Improve and simplify DevContainer, Breakout and SwrHeader to match SWR reference better; allow dar mode switch through storybook; add package:watch script
    
    * Minor layout fix to layout fix :)
    
    * More SwrHeader layout fixes
    
    * Publish prerelease
    
    * Resolve dependency conflicts (https://github.com/npm/cli/issues/4828)
    
    * Fix test (whitespace issues in matcher)
    
    * Reset version number (no prerelease)
    
    ---------
    
    Co-authored-by: Simon Jockers <449739+sjockers@users.noreply.github.com>

---
+++ components/src/DevContainer/ArrowRight.svg.svelte
@@ -0,0 +1,10 @@
+<svg width="6" height="14" viewBox="0 0 6 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M1.00065 12.333L4.33398 6.99967L1.00065 1.66634"
+		stroke="black"
+		stroke-opacity="0.2"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
