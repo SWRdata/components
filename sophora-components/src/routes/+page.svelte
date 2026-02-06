02973a13033b557d827c192c146d439b99f37332
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-02-06 13:59:23 +0100

    Update README and overview page

--- sophora-components/src/routes/+page.svelte
+++ sophora-components/src/routes/+page.svelte
@@ -1,14 +1,24 @@
 <script>
 	import { ChartList, DesignTokens } from '@swr-data-lab/components';
+	import { dev } from '$app/environment';
+
+	// Example parameters for the charts, can be used in development to prefill the charts with data
+	// TODO: Find a better way to manage example parameters, e.g. using Storybook or a list of example URLs
+	const EXAMPLE_PARAMS = {
+		HIGHLIGHT_CARDS:
+			'?data=%5B%0A%20%20%7B%0A%20%20%20%20%22topline%22%3A%20%22Im%20Jahr%202026%20wurden%20bisher%22%2C%0A%20%20%20%20%22value%22%3A%20%220%22%2C%0A%20%20%20%20%22unit%22%3A%20%22%E2%82%AC%22%2C%0A%20%20%20%20%22subline%22%3A%20%22an%20Parteien%20gespendet%22%0A%20%20%7D%2C%0A%20%20%7B%0A%20%20%20%20%22topline%22%3A%20%22Am%2023.%20Dezember%20gingen%22%2C%0A%20%20%20%20%22value%22%3A%20%22136.000%22%2C%0A%20%20%20%20%22unit%22%3A%20%22%E2%82%AC%22%2C%0A%20%20%20%20%22subline%22%3A%20%22an%20die%20Partei%20CSU.%20Absender%20war%20Verband%20der%20Bayrischen%20Metall-%20und%20Elektro-Industrie%22%0A%20%20%7D%0A%5D',
+		DATAWRAPPER_SWITCHER:
+			'?ids=oKR1L,bySDe,4r4Vi&labels=Super,E10,Diesel&fixedHeight=450&layout=row'
+	};
 
 	const charts = [
 		{
 			title: 'Highlight Cards',
-			slug: 'highlight-cards?data=%5B%0A%20%20%7B%0A%20%20%20%20%22topline%22%3A%20%22Im%20Jahr%202026%20wurden%20bisher%22%2C%0A%20%20%20%20%22value%22%3A%20%220%22%2C%0A%20%20%20%20%22unit%22%3A%20%22%E2%82%AC%22%2C%0A%20%20%20%20%22subline%22%3A%20%22an%20Parteien%20gespendet%22%0A%20%20%7D%2C%0A%20%20%7B%0A%20%20%20%20%22topline%22%3A%20%22Am%2023.%20Dezember%20gingen%22%2C%0A%20%20%20%20%22value%22%3A%20%22136.000%22%2C%0A%20%20%20%20%22unit%22%3A%20%22%E2%82%AC%22%2C%0A%20%20%20%20%22subline%22%3A%20%22an%20die%20Partei%20CSU.%20Absender%20war%20Verband%20der%20Bayrischen%20Metall-%20und%20Elektro-%22%0A%20%20%7D%0A%5D'
+			slug: `highlight-cards${dev ? EXAMPLE_PARAMS.HIGHLIGHT_CARDS : ''}`
 		},
 		{
 			title: 'Datawrapper Switcher',
-			slug: 'datawrapper-switcher?ids=oKR1L,bySDe,4r4Vi&labels=Super,E10,Diesel&fixedHeight=450&activeColor=%2305556a'
+			slug: `datawrapper-switcher${dev ? EXAMPLE_PARAMS.DATAWRAPPER_SWITCHER : ''}`
 		}
 	];
 </script>
@@ -16,7 +26,7 @@
 <DesignTokens theme="auto">
 	<ChartList
 		baseUrl="https://static.datenhub.net/apps/sophora-components/main"
-		project="Client-side components for Sophora"
+		project="Sophora-Components"
 		{charts}
 	/>
 </DesignTokens>
