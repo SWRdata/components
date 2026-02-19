5044b7f8985c33ab338d0e4239ee77774025b073
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-02-19 20:13:01 +0100

    Refactor: Use getDataFromUrl helper

--- sophora-components/src/routes/highlight-cards/HighlightCards.svelte
+++ sophora-components/src/routes/highlight-cards/HighlightCards.svelte
@@ -1,31 +1,14 @@
 <script>
+	import getDataFromUrl from '$lib/utils/getDataFromUrl';
 	import { DesignTokens, HighlightCard } from '@swr-data-lab/components';
 	import { onMount } from 'svelte';
 
 	let root;
 	let entries = [];
 
-	const getData = (target) => {
-		let url;
-		if (
-			import.meta.env.DEV ||
-			window.location.origin === 'https://static.datenhub.net' ||
-			window.location.href.includes('apidata.googleusercontent.com') ||
-			window.location.href.includes('storage.googleapis.com')
-		) {
-			// Preview mode – use URL of current page
-			url = new URL(window.location.href);
-		} else {
-			// Embedded mode – use URL used to embed the component
-			const parent = target.parentNode.parentNode;
-			url = new URL(parent?.dataset.url);
-		}
-		const params = Object.fromEntries(url.searchParams);
-		return params.data ? JSON.parse(params.data) : [];
-	};
-
 	onMount(() => {
-		entries = getData(root);
+		const { data } = getDataFromUrl(root);
+		entries = data ? JSON.parse(data) : [];
 	});
 </script>
 
