63c7b7d8b6d7c81d768480d3553ab233d28bb056
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-02-19 20:43:40 +0100

    fix(sophora-components): Fix layout issues in `HighlightCards`  (#314)
    * Extract into separate component, mock website container during development
    
    * Simplify layout, fix sizing issues
    
    * Refactor: Use getDataFromUrl helper
    
    * Smaller grid gap, better layout fit

---
+++ sophora-components/src/routes/highlight-cards/HighlightCards.svelte
@@ -0,0 +1,39 @@
+<script>
+	import getDataFromUrl from '$lib/utils/getDataFromUrl';
+	import { DesignTokens, HighlightCard } from '@swr-data-lab/components';
+	import { onMount } from 'svelte';
+
+	let root;
+	let entries = [];
+
+	onMount(() => {
+		const { data } = getDataFromUrl(root);
+		entries = data ? JSON.parse(data) : [];
+	});
+</script>
+
+<DesignTokens>
+	<div class="highlight-cards" bind:this={root}>
+		{#each entries as entry}
+			<HighlightCard
+				topline={entry.topline}
+				value={entry.value}
+				unit={entry.unit}
+				subline={entry.subline}
+			/>
+		{/each}
+	</div>
+</DesignTokens>
+
+<style>
+	.highlight-cards {
+		display: grid;
+		grid-template-columns: minmax(0, 1fr);
+		gap: 1rem;
+		margin: 0.5rem 0;
+
+		@media (min-width: 1200px) {
+			grid-template-columns: repeat(2, minmax(0, 1fr));
+		}
+	}
+</style>
