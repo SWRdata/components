37325143279f16fe1ffe69f021b59abbcb70f746
Simon Jockers <449739+sjockers@users.noreply.github.com>
2026-04-01 17:38:03 +0200

    Use alternative rendering hook

--- sophora-components/src/hooks.server.js
+++ sophora-components/src/hooks.server.js
@@ -8,7 +8,7 @@ export async function handle({ event, resolve }) {
 				.replace(`id="data-lab-components-embed"`, `data-lab-components-embed="${containerID}"`)
 				.replace(
 					'document.currentScript.parentElement',
-					`document.querySelector("[data-lab-components-embed='${containerID}']")`
+					'document.currentScript.closest("[data-lab-components-embed]")'
 				)
 	});
 	return response;
